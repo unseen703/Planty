3460a79f09bb33d8ec57dc2a6553515fe04a36a6
Dipak <103515582+unseen703@users.noreply.github.com>
2023-07-31 16:34:40 +0530

    Update index.js

--- front_end/src/api/index.js
+++ front_end/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 
-// const API = axios.create({ baseURL: 'http://localhost:5000/' });
-const API = axios.create({ baseURL: 'https://planty-wsml.onrender.com/' });
+const API = axios.create({ baseURL: 'http://localhost:5000/' });
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
